7ceddc7425904f73e3663147959cd75add1e762b
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-01 17:57:36 -0400

    feat: configure TypeORM with PostgreSQL and add user, role, and auth modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
+import { RoleModule } from "./role/role.module";
+import { AuthModule } from "./auth/auth.module";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [__dirname + "/**/*.entity{.ts,.js}"],
+            synchronize: true,
+            logging: true,
+        }),
+        UserModule,
+        RoleModule,
+        AuthModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
