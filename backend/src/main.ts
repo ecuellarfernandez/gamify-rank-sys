a59752f9a45dc42d6d47d38fee476db9c1032586
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-01 22:04:05 -0400

    feat: enable CORS in the bootstrap function for improved API access control

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: process.env.CORS_ORIGIN,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    });
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
