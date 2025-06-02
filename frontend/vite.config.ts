5a075df06b93459ee476ec540303b019e795a564
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-02 18:07:04 -0400

    feat: add @types/node for TypeScript support and configure path alias in Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
